a85c4be249a5cbf4381cec6b614c7caedbea1c1e
morganric <morganric@gmail.com>
2015-12-16 16:48:46 +0100

    icon

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy, :plays, :embed]
-  before_filter :authenticate_user!,  except: [:index, :show, :tag, :featured, :embed]
+  before_filter :authenticate_user!,  except: [:index, :show, :tag, :featured, :embed, :tag, :author, :provider]
   
 
    require 'embedly'
