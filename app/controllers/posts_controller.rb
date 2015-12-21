e67e5c08b1f9ab29c6b7b9c493c40259a6047eb8
morganric <morganric@gmail.com>
2015-12-21 01:04:16 +0000

    fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order('plays ASC')
     @featured = Post.where(:featured => true) 
     @new = Post.all.order('created_at ASC')
   end
