b6dab27fe9ec2ce0f53ecaf860f0f58a400d5bf3
morganric <morganric@gmail.com>
2015-12-29 22:24:55 +0000

    fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy, :plays, :embed, :popout]
-  before_filter :authenticate_user!,  except: [ :show, :tag, :featured, :embed, 
+  before_filter :authenticate_user!,  except: [ :show, :featured, :embed, 
                     :tag, :author, :provider, :popout, :latest]
-   before_action :admin_only, :except => [:embed, :create, :destroy, :show, :page, :popular, 
-                :edit, :index, :favourites, :update, :featured]
+   before_action :admin_only, :except => [:embed, :destroy, :show, :page, :popular, :tag
+                :edit, :index, :favourites, :update, :featured, :popout, :latest, :provier, :author]
 
    require 'embedly'
   require 'json'
