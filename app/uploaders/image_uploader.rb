7fbc22cd77efbe34d6b6357d54cb2db0b5d08a83
morganric <morganric@gmail.com>
2016-01-10 22:02:27 +0000

    fav

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -46,6 +46,10 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   end
 
+
+  version :thumbnail do
+    process :resize_to_fill => [100, 100]
+  end  
   
    version :avatar do
     process :resize_to_fill => [25, 25]
